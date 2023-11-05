d34e042c01ef7147eefeed71acff9a0ab8ba091b
TABRILLA <angelestabrilla10@gmail.com>
2023-11-05 20:59:25 +0800

    done heading

--- src/pages/Home/Heading.tsx
+++ src/pages/Home/Heading.tsx
@@ -1,8 +1,29 @@
-
+import { Button } from "@material-tailwind/react";
 
 const Heading = () => {
+  const AutoCenter = "max-w-[1250px] mt-[-96px] w-full h-screen flex flex-col justify-center";
+  const FontBoldCenter = "text-center font-bold text-white";
+  const ButtonColor ="bg-[#f9fbe7] text-black font-bold hover:bg-[#689f38] hover:text-white";
+
   return (
-    <div>Heading</div>
+    <section className="bg-secondary font-serif">
+      <div className={`${AutoCenter} mx-auto`}>
+        
+        <p className={`${FontBoldCenter} p-16 text-7xl`}>
+          STI Student Council Election 
+        </p>
+
+        <p className={`${FontBoldCenter} px-24 text-2xl leading-loose`}>
+          Vote for a brighter tomorrow, where students' voices shape our future. Together, we're the architects of change in the STI community.
+        </p>
+
+        <div className={`${FontBoldCenter} mx-auto  py-10 flex w-max items-end gap-4`}>
+          <Button size="lg" className={`${ButtonColor}`}>Start Vote</Button>
+          <Button size="lg" className={`${ButtonColor}`}>View More</Button>
+        </div>
+      </div>
+        
+    </section>
   )
 }
 
